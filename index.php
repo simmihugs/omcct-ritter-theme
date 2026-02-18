61c39bfc7404f0f54a51f7e4a587dead12651063
simmihugs <simon.graetz@gmx.de>
2026-02-18 20:51:44 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<main class="site-content">
+  <?php get_header(); ?>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+  <h1>Willkommen auf der Homepage unseres Ritterordens!</h1>
+  <p>Der Aufbau hat begonnen.</p>
+
+</main>
+
+<?php wp_footer(); ?>

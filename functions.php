a552cd49143ebd5761ca7817f9a8f1814e9795e5
simmihugs <simon.graetz@gmx.de>
2026-02-18 22:10:57 +0100

    fixed zeilenumbruch im Wahlspruch

--- functions.php
+++ functions.php
@@ -1,9 +1,7 @@
 <?php
 function omcct_setup() {
-    // Titel-Unterstützung
     add_theme_support('title-tag');
 
-    // Menü-System freischalten
     register_nav_menus(array(
         'main-menu' => 'Hauptmenü Oben',
     ));
@@ -11,7 +9,6 @@ function omcct_setup() {
 add_action('after_setup_theme', 'omcct_setup');
 
 function omcct_scripts() {
-    // Haupt-Stylesheet laden
     wp_enqueue_style('omcct-main-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'omcct_scripts');
